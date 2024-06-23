29296b05f3558ad4ba84b6d17591089de601110b
ninadwastaken <ninad2eyes@gmail.com>
2024-06-24 00:06:49 +0530

    asda

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Too good</Text>
+      <Text>Too apksjdsaokd</Text>
       <StatusBar style="auto" />
     </View>
   );
